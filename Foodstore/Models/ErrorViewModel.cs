6697b675b9aa9d87b646535b25ac4d653b18a784
Pavel <Anya@LAPTOP-K4AVO9IM>
2025-04-12 19:23:48 +0300

    Добавьте файлы проекта.

---
+++ Foodstore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Foodstore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
